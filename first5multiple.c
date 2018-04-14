c441b970b5bcac5a998d0fe2ca4858155286e737
sagar123agrawal <30861176+sagar123agrawal@users.noreply.github.com>
2018-04-14 14:19:23 +0530

    Create first5multiple.c

---
+++ first5multiple.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+  int n,i,r;
+  scanf("%d",&n);
+  for(i=1;i<6;i++)
+  {
+   r=n*i;
+   printf("%d ",r);
+   }
+   return 0;
+   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
